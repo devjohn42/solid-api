2b74decf9f4d34d806a6d25ae8aa10e2145de5f2
devjohn42 <devjohncb@gmail.com>
2025-04-29 00:05:12 -0300

    :wrench: chore: sets environment variables

---
+++ src/env/index.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333)
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('🚨 Invalid enviroment variables', _env.error.format())
+
+  /*
+    Top Level da aplicação, aso dê algum problema nas variáveis,
+    a aplicação será derrubada
+  */
+  throw new Error('Invalid enviroment variables')
+}
+
+export const env = _env.data
+
