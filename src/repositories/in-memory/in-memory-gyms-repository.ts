cdd14d72d4f001a8bc1344c64a023db6cd9419f9
devjohn42 <devjohncb@gmail.com>
2025-05-16 20:58:16 -0300

    :sparkles: feat: creates the servide to get nearby gyms

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,19 +1,10 @@
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 import { Gym, Prisma } from 'generated/prisma'
 import { randomUUID } from 'node:crypto'
-import { GymsRepository } from '../gyms-repository'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = []
-
-  async findById(id: string): Promise<Gym | null> {
-    const gym = this.items.find((item) => item.id === id)
-
-    if (!gym) {
-      return null
-    }
-
-    return gym
-  }
 
   async create(data: Prisma.GymCreateInput) {
     const gym = {
@@ -31,9 +22,33 @@ export class InMemoryGymsRepository implements GymsRepository {
     return gym
   }
 
+  async findById(id: string): Promise<Gym | null> {
+    const gym = this.items.find((item) => item.id === id)
+
+    if (!gym) {
+      return null
+    }
+
+    return gym
+  }
+
   async searchMany(query: string, page: number) {
     return this.items
       .filter((item) => item.title.includes(query))
       .slice((page - 1) * 20, page * 20)
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        },
+      )
+
+      return distance < 10
+    })
   }
 }
