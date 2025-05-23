5cbe47d575c4420971b8576403d72f744ba049b1
devjohn42 <devjohncb@gmail.com>
2025-05-23 00:34:10 -0300

    :white_check_mark: test: adds RBAC to tests

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,11 +1,19 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post('/users').send({
-    name: 'John',
-    email: 'john@gmail.com',
-    password: 'fap@2025',
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'John',
+      email: 'john@gmail.com',
+      password_hash: await hash('fap@2025', 4),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
